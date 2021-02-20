f2284c4b4d72bbd1882e5a8ca06963f3c2ee243f
Faiz Surani <faiz.surani@gmail.com>
2021-02-19 18:01:07 -0800

    Add portfolio value helper method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,13 @@ class User < ApplicationRecord
   has_many :transactions
   has_many :portfolio_value_histories
   has_many :exchanges
+
+  def portfolio_value
+    total_value = self.cash_balance
+    portfolio_ownerships = self.ownerships.includes(:stock)
+    portfolio_ownerships.each do |ownership|
+      total_value += ownership.num_shares * ownership.stock.share_price
+    end
+    total_value
+  end
 end
