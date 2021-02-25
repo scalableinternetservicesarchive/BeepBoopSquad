c1763bf8040ba5839f7cd9ef7eff39acddcbe125
Ben Ye <z_ye@ucsb.edu>
2021-02-25 01:47:30 -0800

    fixed some bugs

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -13,24 +13,21 @@ class TransactionsController < ApplicationController
 
   # POST /transactions or /transactions.json
   def create
-    user = nil
-    if !transaction_params.key?(:user_id) || transaction_params[:user_id].nil?
-      user = User.find(session[:user_id])
-    else
-      user = User.find(user_id)
-    end
-
-    stock = nil
-    if transaction_params[:stock_id].is_a?(String)
-      stock = Stock.find_by_symbol(transaction_params[:stock_id].upcase)
-    else
-      stock = Stock.find(stock_id)
-    end
+    user = User.find(transaction_params[:user_id])
+    stock = Stock.find(transaction_params[:stock_id])
     if user.nil? || stock.nil?
-      return render json: {error: "Specified user or stock did not exist"}, status: :unprocessable_entity
+      respond_to do |format|
+      format.html {redirect_to root_path, notice: "Specified user or stock did not exist", status: :unprocessable_entity }
+      format.json { render json: {error: "Specified user or stock did not exist"}, status: :unprocessable_entity }
+      end
+    end
+    if params.has_key?(:formats)
+      params[:format] = :html
+      request.format = :html
     end
     num_shares = transaction_params[:num_shares].to_i
     transaction_amount = num_shares * stock.share_price
+
     ownership_record = Ownership.find_or_initialize_by(stock: stock, user: user)
     if ownership_record.num_shares.nil?
       ownership_record.num_shares = 0
@@ -41,27 +38,38 @@ class TransactionsController < ApplicationController
     when "buy"
       @transaction.transaction_type = :buy
       if user.cash_balance < transaction_amount
-        return render json: {error: "Not enough money to complete the transaction"}, status: :forbidden
+        if request.format == :html
+          return redirect_to new_trade_path, notice: "Not enough money to complete the transaction"
+        elsif request.format == :json
+          return render json: {error: "Not enough money to complete the transaction"}, status: :forbidden
+        end
       end
       ownership_record.num_shares += num_shares
       user.cash_balance -= transaction_amount
     when "sell"
       @transaction.transaction_type = :sell
       if ownership_record.num_shares < num_shares
-        return render json: {error: "Not enough shares owned to sell."}, status: :forbidden
+        if request.format == :html
+          return redirect_to new_trade_path, notice: "Not enough shares owned to sell."
+        elsif request.format == :json
+          return render json: {error: "Not enough shares owned to sell."}, status: :forbidden
+        end
       end
       ownership_record.num_shares -= num_shares
       user.cash_balance += transaction_amount
     else
-      return render json: {error: "Invalid transaction type."}, status: :unprocessable_entity
+      if request.format == :html
+        return redirect_to new_trade_path, notice: "Invalid transaction type."
+      elsif request.format == :json
+        return render json: {error: "Invalid transaction type."}, status: :unprocessable_entity
+      end
     end
-
     respond_to do |format|
       if @transaction.save && user.save && ownership_record.save
-        format.html { redirect_to @transaction, notice: "Transaction was successfully created." }
+        format.html { redirect_to root_path, notice: "Transaction was successfully created." }
         format.json { render :show, status: :created, location: @transaction }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render new_trade_path, notice: "Transaction unsuccessful. Please try again" }
         format.json { render json: @transaction.errors, status: :unprocessable_entity }
       end
     end
