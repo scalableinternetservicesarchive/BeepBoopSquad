c9c854d55dc77ae6d4c4c61ef60b6e1c55a1b348
Ben Ye <z_ye@ucsb.edu>
2021-02-25 01:47:30 -0800

    fixed some bugs

--- app/controllers/trade_controller.rb
+++ app/controllers/trade_controller.rb
@@ -2,6 +2,35 @@ class TradeController < ApplicationController
   def index
   end
 
+  def create
+    #Parameters: {"authenticity_token"=>"[FILTERED]", "transaction_type"=>"sell", "stock_id"=>"", "selected_stock"=>"1", "num_shares"=>"1", "commit"=>"trade"}
+    #parse time! bee do bee do
+    input_params = Hash.new
+    input_params[:transaction] = Hash.new
+    
+    trade_selection = params[:transaction_type]
+    stock_id = -1
+    if trade_selection == "buy"
+      stock_id = Stock.find_by_symbol(params[:stock_id])
+      if stock_id.nil?
+        respond_to do |format|
+        format.html { redirect_to new_trade_path, notice: "Unable to find stock symbol" }
+        end
+        return
+      else
+        stock_id = stock_id.id
+      end
+    elsif trade_selection == "sell"
+      stock_id = params[:selected_stock]
+    end
+    input_params[:transaction][:transaction_type] = trade_selection
+    input_params[:transaction][:stock_id] = stock_id
+    input_params[:transaction][:num_shares] = params[:num_shares]
+    input_params[:transaction][:user_id] = session[:user_id]
+    input_params[:formats] = :html
+    redirect_post(transactions_path, params: input_params)
+  end
+
 =begin
 <!--
 <form>
