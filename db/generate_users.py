93367d81933b54a7a408f37c18cac923f5a2e734
Ben Ye <z_ye@ucsb.edu>
2021-03-06 15:39:32 -0800

    seeded users

---
+++ db/generate_users.py
@@ -0,0 +1,16 @@
+import csv
+import random
+from random_username.generate import generate_username
+print("Input # of users you want to generate")
+val = int(input())
+names = generate_username(val)
+data = {}
+import csv
+
+with open('data/users.csv', mode='w') as user_file:
+    user_writer = csv.writer(user_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    user_writer.writerow(['Name', 'Password', 'Cash_Balance'])
+    for i in range(val):
+        user_writer.writerow([names[i], 'password', random.randint(1000, 100000)])
+    
+    
