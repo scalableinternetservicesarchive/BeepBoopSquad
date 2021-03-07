973a5a541714fe607e98902f3f1440908bae6ab6
Ben Ye <z_ye@ucsb.edu>
2021-03-06 16:54:35 -0800

    reverted some changes

--- db/generate_users.py
+++ db/generate_users.py
@@ -9,7 +9,7 @@ import csv
 
 with open('data/users.csv', mode='w') as user_file:
     user_writer = csv.writer(user_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    user_writer.writerow(['Name', 'Password', 'Cash_Balance'])
+    user_writer.writerow(['Name', 'Cash_Balance'])
     for i in range(val):
         user_writer.writerow([names[i], 'password', random.randint(1000, 100000)])
     
