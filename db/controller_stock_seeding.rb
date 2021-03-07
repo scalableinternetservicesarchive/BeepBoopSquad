d393e433334629ff21760fbbdd9ce8891bc9abdc
Ben Ye <z_ye@ucsb.edu>
2021-03-06 18:16:03 -0800

    added /:id to /seed_stocks and /seed_users endpoints

---
+++ db/controller_stock_seeding.rb
@@ -0,0 +1,19 @@
+require 'csv'
+
+def seed_stocks(id)
+  csv_file_path = Rails.root.join('db', 'data', 'stocks' + id + '.csv')
+  puts 'Seeding stocks from #{csv_file_path}...'
+  f = File.new(csv_file_path, 'r')
+  csv = CSV.new(f)
+  headers = csv.shift
+  
+  csv.each do |row|
+    stock_information = {
+      symbol: row[0],
+      name: row[1],
+      share_price: row[2]
+    }
+    Stock.create_with(stock_information).find_or_create_by(symbol: row[0])
+  end
+  puts 'Seeding stocks from #{csv_file_path} done.'
+end
